0f214c756de853188102f8b2c2de533c3155cd06
Vinz <vinz@gmail.com>
2023-09-02 01:50:40 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+$("#imageGallery").imagePopup({
+    overlay: "rgba(0, 0, 0, 0.5)",
+    closeButton:{
+        // src: "images/close.png",
+        width: "30px",
+        height: "40px"
+    },
+    imageBorder: "5px solid #fffff",
+    borderRadius: "5px",
+    imageWidth: "500px",
+    imageHeight: "400px"
+    
+});
